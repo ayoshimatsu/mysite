52542ee929d1bda6cba2aecefe04b4d8181fb06f
ayoshimatsu <yoshimaster7@gmail.com>
2020-08-30 19:14:15 +0900

    Add easy diary

--- diary/views.py
+++ diary/views.py
@@ -1,7 +1,23 @@
+import logging
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
+from .forms import InquiryForm
 
-app_name = "diary"
+logger = logging.getLogger(__name__)
+
 class IndexView(generic.TemplateView):
     template_name = "diary/index.html"
+
+class InquiryView(generic.FormView):
+    template_name = "diary/inquiry.html"
+    form_class = InquiryForm
+    success_url = reverse_lazy("diary:inquiry")
+
+    def form_valid(self, form):
+        form.send_email()
+        logger.info("Inquiry sent by {}".format(form.cleaned_data["name"]))
+        return super().form_valid(form)
+
+
 
